e8d1b2d77f61a3403a6913c3aff5ba1e9dc90341
Alexander Shushlin <alexshushlin@gmail.com>
2022-09-19 14:11:46 +0300

    add First Work of Ninth HomeWork

---
+++ Seminar9_HomeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+//Запрос данных от пользователя
+int ReadData(string line)
+{
+    Console.Write(line);
+    int num = int.Parse(Console.ReadLine() ?? "1");
+    return num;
+}
+
+// Вывод данных
+void PrintData(string prefix, string num)
+{
+    Console.WriteLine(prefix + num);
+}
+
+// Рекурсия числа от m до n
+string IntervalMN(int m, int n)
+{
+    // точка остановки рекурсии
+    if( m>=n ) return n.ToString();
+    // рекурентный выход метода
+    return m + " " + IntervalMN(m+1,n);
+}
+
+// Ввод данных и вывод результата с проверкой условия
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+string res = (m < n) ? IntervalMN(m,n) : IntervalMN(n,m);
+PrintData("Числа в заданном промежутке: ",res);
+
