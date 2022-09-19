c7912f1171a6a964eaf9bf108e9753ebed57a26c
Alexander Shushlin <alexshushlin@gmail.com>
2022-09-19 14:14:11 +0300

    add Second Work of Ninth HomeWork

--- Seminar9_HomeWork/Program.cs
+++ Seminar9_HomeWork/Program.cs
@@ -16,18 +16,39 @@ void PrintData(string prefix, string num)
     Console.WriteLine(prefix + num);
 }
 
-// Рекурсия числа от m до n
-string IntervalMN(int m, int n)
+// // Рекурсия числа от m до n
+// string IntervalMN(int m, int n)
+// {
+//     // точка остановки рекурсии
+//     if( m>=n ) return n.ToString();
+//     // рекурентный выход метода
+//     return m + " " + IntervalMN(m+1,n);
+// }
+
+// // Ввод данных и вывод результата с проверкой условия
+// int m = ReadData("Введите число M: ");
+// int n = ReadData("Введите число N: ");
+// string res = (m < n) ? IntervalMN(m,n) : IntervalMN(n,m);
+// PrintData("Числа в заданном промежутке: ",res);
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+//Рекурсивный расчет суммы чисел от m до n
+int SumMN(int m, int n)
 {
-    // точка остановки рекурсии
-    if( m>=n ) return n.ToString();
-    // рекурентный выход метода
-    return m + " " + IntervalMN(m+1,n);
+       //точка остановки рекурсии     
+    if (m >= n) return n;
+       //рекурсивный расчет суммы и ее вывод 
+    return m + SumMN(m+1,n);
 }
 
-// Ввод данных и вывод результата с проверкой условия
+// Ввод значений и вывод результата с проверкой условия
 int m = ReadData("Введите число M: ");
 int n = ReadData("Введите число N: ");
-string res = (m < n) ? IntervalMN(m,n) : IntervalMN(n,m);
-PrintData("Числа в заданном промежутке: ",res);
+int res = (m < n) ? SumMN(m,n) : SumMN(n,m);
+PrintData("Сумма чисел в заданном промежутке: ",res.ToString());
+
 
